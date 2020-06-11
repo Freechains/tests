c16f459beb495509dc3c6618578ae622e7f10dc9
fsantanna <francisco.santanna@gmail.com>
2020-06-11 19:59:03 -0300

    (>) artifacts

--- src/main/shell/test/tests.sh
+++ src/main/shell/test/tests.sh
@@ -2,8 +2,9 @@
 
 #while :
 #do
-  ./general.sh || exit 1
-  ./sync.sh    || exit 1
-  ./pubpvt.sh  || exit 1
-  ./like.sh    || exit 1
+  ./general.sh   || exit 1
+  ./sync.sh      || exit 1
+  ./pubpvt.sh    || exit 1
+  ./like.sh      || exit 1
+  ./bootstrap.sh || exit 1
 #done
